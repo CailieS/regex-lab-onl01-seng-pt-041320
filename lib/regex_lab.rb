a9f2b7189797c0061aabbe45ffe158b9a33b437d
Cailie Skelton <cskelton707@gmail.com>
2020-04-29 17:21:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,6 @@ def starts_with_a_vowel?(word)
     return true
   end
 end
-starts_with_a_vowel?("afoot","Excellent","incredible","Absolute","unreal","Inconceivable","octopus")
 
 def words_starting_with_un_and_ending_with_ing(text)
 
