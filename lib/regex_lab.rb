2b4df76217c3668bfd17cb38fba4fe8078c8e961
Cailie Skelton <cskelton707@gmail.com>
2020-04-29 17:13:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,11 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/[AEIOU][aeiou]/) == nil
+    return false
+  else
+    return true
+  end
 end
+starts_with_a_vowel?("afoot","Excellent","incredible","Absolute","unreal","Inconceivable","octopus")
 
 def words_starting_with_un_and_ending_with_ing(text)
 
